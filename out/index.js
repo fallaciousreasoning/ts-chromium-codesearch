ecce0fb8be15697455054a090cb5ae8fc1f1daab
Jay Harris <jay.harris@outlook.co.nz>
2019-07-09 17:16:17 +1000

    removes out

--- out/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var annotationsHelper_1 = require("./codesearch/annotationsHelper");
-annotationsHelper_1.getAnnotationsForFile('src/chrome/browser/ui/web_applications/app_browser_controller.h').then(console.log);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { getAnnotationsForFile } from "./codesearch/annotationsHelper";
 
-getAnnotationsForFile('src/chrome/browser/ui/web_applications/app_browser_controller.h').then(console.log)
+const main = async () => {
+    const annotations = await getAnnotationsForFile('src/chrome/browser/ui/web_applications/app_browser_controller.h');
+}
+
+main();
